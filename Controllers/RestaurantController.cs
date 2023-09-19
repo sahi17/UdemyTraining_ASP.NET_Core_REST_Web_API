b81a4096c7388862ef0c693dbe478ddb57da6a37
MPIEKARCZYK <mpiekarczyk@senga.com.pl>
2023-09-19 13:59:54 +0200

    Lesson 18 completed

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UdemyTraining_ASP.NET_Core_REST_Web_API.Entities;
+
+namespace UdemyTraining_ASP.NET_Core_REST_Web_API.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restaurants = _dbContext.Restaurants.ToList();
+            return Ok(restaurants);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute] int id)
+        {
+            var restaurant = _dbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+    }
+}
--- TemperatureRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UdemyTraining_ASP.NET_Core_REST_Web_API
-{
-    public class TemperatureRequest
-    {
-        public int Min { get; set; }
-        public int Max { get; set; }
-    }
-}
